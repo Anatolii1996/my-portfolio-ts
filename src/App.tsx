a707c956f952a136b208f65752a912067fbea699
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2023-11-08 11:31:58 +0200

    add yeti to notFound page

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import HeaderWrap from "./components/Header/HeaderWrap";
 import Feedback from "./pages/Feedback/Feedback";
 import "animate.css";
 import Footer from "./components/Footer/Footer";
+import NotFound from "./pages/NotFound/NotFound";
 
 const App: FC = () => {
   return (
     <Routes>
+      <Route path="*" element={<NotFound/>} />
       <Route path="/" element={<HeaderWrap />}>
         <Route path="/" element={<Footer />}>
           <Route path="/feedback" element={<Feedback />} />
