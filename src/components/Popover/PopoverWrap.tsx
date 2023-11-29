201a547ab4425a0a10fc9aa51d62a5cf4b982e76
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2023-11-29 21:44:20 +0200

    add style to popover

--- src/components/Popover/PopoverWrap.tsx
+++ src/components/Popover/PopoverWrap.tsx
@@ -1,6 +1,7 @@
-import React, { useMemo, useState, FC } from "react";
-import { Button, ConfigProvider, Popover, Segmented } from "antd";
+import React, { FC } from "react";
+import {  ConfigProvider, Popover } from "antd";
 import { IPopover } from "./types";
+import "./popover.scss";
 
 const text = <span>Technologies:</span>;
 
@@ -8,7 +9,8 @@ const text = <span>Technologies:</span>;
 
 const PopoverWrap: FC<IPopover> = ({ content, children, comment }) => {
   const contentItems = (
-    <>
+    <div className="popover_content" 
+    >
       <div>
         {content.map((el) => {
           return <p key={el}>{el}</p>;
@@ -22,14 +24,16 @@ const PopoverWrap: FC<IPopover> = ({ content, children, comment }) => {
           })}
         </div>
       )}
-    </>
+    </div>
   );
 
   return (
-    <ConfigProvider>
+    <ConfigProvider >
       <div className="demo">
         <div>
-          <Popover placement="right" title={text} content={contentItems}>
+          <Popover
+          //  visible={true}
+            placement="right" title={text} content={contentItems} >
             {children}
           </Popover>
         </div>
