08d2b6a2853fa36c18915e7be2d7fbe68f1f4dc5
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2023-12-23 21:11:19 +0200

    fix bug with date comment

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,49 +1,48 @@
 import React, { FC } from "react";
 import { useAppSelector } from "../../hooks";
-import { v4 as uuidv4 } from "uuid"
-import {  Empty } from 'antd';
+import { v4 as uuidv4 } from "uuid";
+import { Empty } from "antd";
 import cn from "classnames";
+import moment from "moment";
 import "./chat.scss";
 
 const Chat: FC = () => {
-  const comments = useAppSelector((state)=>state.comments.messages)
-console.log(Boolean(comments) )
+  const comments = useAppSelector((state) => state.comments.messages);
+  // console.log(Boolean(comments) )
   return (
     <div className="chat_wrap animate__animated animate__fadeInRightBig">
-      {comments.length ?
-       comments.map((comment, index) => {
-        // console.log(comment)
-        // const messageId = comment._id;
-        const messageClasses = cn("chat_message", {
-          my_message: comment.ipAddress === process.env.REACT_APP_MY_IP,
-          "animate__animated animate__fadeInRightBig": index === 0,
-        });
-        return (
-          <div key={uuidv4()} className={messageClasses}>
-            <div className="message_author">
-              <div className="author_name">
-                <p>{comment.name}</p>
-                <p>{comment.surname}</p>
+      {comments.length ? (
+        comments.map((comment, index) => {
+          // console.log(comment)
+          // const messageId = comment._id;
+          const messageClasses = cn("chat_message", {
+            my_message: comment.ipAddress === process.env.REACT_APP_MY_IP,
+            "animate__animated animate__fadeInRightBig": index === 0,
+          });
+          return (
+            <div key={uuidv4()} className={messageClasses}>
+              <div className="message_author">
+                <div className="author_name">
+                  <p>{comment.name}</p>
+                  <p>{comment.surname}</p>
+                </div>
+                <p>
+                  {comment.date
+                    ? comment.date
+                    : moment().format("DD.MM.YYYY HH:mm")}
+                </p>
               </div>
-              <p>{comment.date}</p>
+              <p>{comment.comment}</p>
             </div>
-            <p>{comment.comment}</p>
-          </div>
-        );
-      }):
-      <Empty
-      image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-      imageStyle={{ height: 60 }}
-      description={
-        <span>
-          Не вдалося завантажити  коментарі
-        </span>
-      }
-    >
-      
-    </Empty>
-      }
-
+          );
+        })
+      ) : (
+        <Empty
+          image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+          imageStyle={{ height: 60 }}
+          description={<span>Не вдалося завантажити коментарі</span>}
+        ></Empty>
+      )}
     </div>
   );
 };
