898eb1d5b8774bd78a9608c244def43d151ff97a
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-01-14 21:41:32 +0200

    add style

--- src/pages/NoAcces/NoAcces.tsx
+++ src/pages/NoAcces/NoAcces.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./noAcces.scss";
 
 const NoAcces = () => {
-  return <div>NoAcces</div>;
+  return (
+    <div className="noAcces_page">
+      <div className="noAcces_message">
+        <p>Вам заборонено доступ на даний ресурс, за комунікацію, що не відповідає характеру дружнього спілкування!</p>
+        <p>You are banned from this resource for communication that is not in line with the nature of friendly communication!</p>
+      </div>
+    </div>
+  );
 };
 
 export default NoAcces;
